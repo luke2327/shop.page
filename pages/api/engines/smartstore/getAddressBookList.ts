95ffcd5e5d8d86adddec7537842b9c47018b7436
luke2327 <qwepoi8526@gmail.com>
2023-09-08 11:05:40 +0900

    feat: test 6

--- pages/api/engines/smartstore/getAddressBookList.ts
+++ pages/api/engines/smartstore/getAddressBookList.ts
@@ -1,31 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { getAddressList, getTokenInfo } from '@/services/tp.service'
 import { openGate } from '@/lib/cors'
 
 type Data = {
-  result: {
-    list: string
-    message: 'SUCCESS'
-  }
+  // result: {
+  //   list: string
+  //   message: 'SUCCESS'
+  // }
+  name: any
 }
 
 export default async function POST(req: NextApiRequest, res: NextApiResponse<Data>) {
   await openGate(req, res)
   if (req.method === 'POST') {
-    console.log('-------------------------')
-    console.log(req.body)
-    console.log('-------------------------')
-    const params = req.body
-    const sendToken = await getTokenInfo(params.receive)
-    console.log(sendToken)
-    const addressList = await getAddressList(sendToken)
-
-    res.status(200).json({
-      result: {
-        list: addressList,
-        message: 'SUCCESS',
-      },
-    })
+    res.status(200).json({ name: req.body })
+    // console.log('-------------------------')
+    // console.log(req.body)
+    // console.log('-------------------------')
+    // const params = req.body
+    // const sendToken = await getTokenInfo(params.receive)
+    // console.log(sendToken)
+    // const addressList = await getAddressList(sendToken)
+    //
+    // res.status(200).json({
+    //   result: {
+    //     list: addressList,
+    //     message: 'SUCCESS',
+    //   },
+    // })
   }
 }
