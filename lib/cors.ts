789b622a4bc10b4d254bc6b6e2d4e82e22be7cc1
luke2327 <qwepoi8526@gmail.com>
2023-09-08 10:21:27 +0900

    feat: mod cors

--- lib/cors.ts
+++ lib/cors.ts
@@ -5,6 +5,7 @@ export const openGate = async (req: NextApiRequest, res: NextApiResponse) => {
   await NextCors(req, res, {
     // Options
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    'Access-Control-Allow-Methods': ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     origin: '*',
     'Content-Type': 'application/json; charset=utf-8',
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
