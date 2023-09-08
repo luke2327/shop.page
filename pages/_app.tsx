f4a427519c8d0e3ad9af2b411a655179712ef423
luke2327 <qwepoi8526@gmail.com>
2023-09-08 09:15:35 +0900

    feat: add app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '@/styles/globals.scss'
+import React from 'react'
+import { ConfigProvider } from 'antd'
 import type { AppProps } from 'next/app'
+import { RecoilRoot } from 'recoil'
+import Layout from '@/components/layout'
+import '@/styles/globals.scss'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <RecoilRoot>
+    <ConfigProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ConfigProvider>
+  </RecoilRoot>
+)
 
-export default MyApp
+export default App
