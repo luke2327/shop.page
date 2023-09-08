000d4192a224e3d2d579cbbe668951bdf9b7e821
luke2327 <qwepoi8526@gmail.com>
2023-09-08 09:14:39 +0900

    feat: add document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,42 @@
+import React from 'react'
+import { StyleProvider, createCache, extractStyle } from '@ant-design/cssinjs'
 import Document, { Head, Html, Main, NextScript } from 'next/document'
+import type { DocumentContext } from 'next/document'
 
-// Need to create a custom _document because i18n support is not compatible with `next export`.
-class GlobalDocument extends Document {
-  // eslint-disable-next-line class-methods-use-this
-  render() {
-    return (
-      <Html lang="en">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
+const MyDocument = () => (
+  <Html lang="en">
+    <Head />
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+)
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const cache = createCache()
+  const originalRenderPage = ctx.renderPage
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) =>
+        (
+          <StyleProvider cache={cache}>
+            <App {...props} />
+          </StyleProvider>
+        ),
+    })
+
+  const initialProps = await Document.getInitialProps(ctx)
+  const style = extractStyle(cache, true)
+  return {
+    ...initialProps,
+    styles: (
+      <>
+        {initialProps.styles}
+        <style dangerouslySetInnerHTML={{ __html: style }} />
+      </>
+    ),
   }
 }
 
-export default GlobalDocument
+export default MyDocument
