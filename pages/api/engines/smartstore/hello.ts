7e351ba013d78479ea9aa44e4d52fa1f5b80eed4
luke2327 <qwepoi8526@gmail.com>
2023-09-08 10:53:11 +0900

    feat: test 4

--- pages/api/engines/smartstore/hello.ts
+++ pages/api/engines/smartstore/hello.ts
@@ -1,12 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import TpService from '@/services/tp.service'
 
 type Data = {
   name: any
 }
 
+const tpService = new TpService()
+
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  // const tpService = new TpService()
   const params = req.body
   res.status(200).json({ name: params })
 }
