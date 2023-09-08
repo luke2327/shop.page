8b4e3048270f5c150b21eb679c76879bb7b6043d
luke2327 <qwepoi8526@gmail.com>
2023-09-08 09:15:12 +0900

    feat: remove file

--- generators/plopfile.js
+++
@@ -1,6 +0,0 @@
-const { page, component } = require('./settings')
-
-module.exports = function (plop) {
-  plop.setGenerator('page', page)
-  plop.setGenerator('component', component)
-}
--- generators/settings/index.js
+++
@@ -1,7 +0,0 @@
-const page = require('./page')
-const component = require('./component')
-
-module.exports = {
-  page,
-  component
-}
