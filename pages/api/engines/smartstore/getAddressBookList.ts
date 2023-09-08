48508849a7989a0e7732b63462c67f972863353e
luke2327 <qwepoi8526@gmail.com>
2023-09-08 11:15:56 +0900

    feat: test 10

--- pages/api/engines/smartstore/getAddressBookList.ts
+++ pages/api/engines/smartstore/getAddressBookList.ts
@@ -14,21 +14,25 @@ type Data = {
 export default async function POST(req: NextApiRequest, res: NextApiResponse<Data>) {
   await openGate(req, res)
   if (req.method === 'POST') {
-    // console.log('-------------------------')
-    // console.log(req.body)
-    // console.log('-------------------------')
-    const params = req.body
-    const sendToken = await getTokenInfo(params.receive)
+    try {
+      // console.log('-------------------------')
+      // console.log(req.body)
+      // console.log('-------------------------')
+      const params = req.body
+      const sendToken = await getTokenInfo(params.receive)
 
-    res.status(200).json({ name: req.body })
-    // console.log(sendToken)
-    // const addressList = await getAddressList(sendToken)
-    //
-    // res.status(200).json({
-    //   result: {
-    //     list: addressList,
-    //     message: 'SUCCESS',
-    //   },
-    // })
+      res.status(200).json({ name: req.body })
+      // console.log(sendToken)
+      // const addressList = await getAddressList(sendToken)
+      //
+      // res.status(200).json({
+      //   result: {
+      //     list: addressList,
+      //     message: 'SUCCESS',
+      //   },
+      // })
+    } catch (e: any) {
+      res.status(200).json({ name: e.toString() })
+    }
   }
 }
